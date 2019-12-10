1ccb17da00b3258131d6b32ee9186a84e7fe566e
Roeland <roeland.oostdam@outlook.com>
2019-12-10 10:49:15 +0100

    Fixed vision, deceleration

--- traffic_simulation/main.py
+++ traffic_simulation/main.py
@@ -2,28 +2,36 @@ from model import CarModel
 import matplotlib.pyplot as plt
 
 # Config
-n = 10 # Amount of model steps
-width = 1000
+n = 400 # Amount of model steps
+car_count = 100 # Amount of cars in model
 height = 1
 acceleration = 1
-deceleration = 1
 vision_range = 100
 speed_limit = 5
-randomization = 0.1
+width = n*speed_limit
+randomization = 0.5
 
 all_speeds = []
-model = CarModel(n, width, height, acceleration, deceleration, vision_range, randomization, speed_limit)
-for i in range(25):
+all_tracked_agent_speeds = []
+model = CarModel(car_count, width, height, acceleration, vision_range, randomization, speed_limit)
+for i in range(n):
     model.step()
 
     # Store the results
     total = 0
+    tracked_agent_total = 0
     for agent in model.schedule.agents:
         total += agent.speed
+        if agent.unique_id==5:
+            tracked_agent_total+=agent.speed
     all_speeds.append(total/model.num_agents)
+    all_tracked_agent_speeds.append(tracked_agent_total)
 
 print('all_speeds:', all_speeds)
 plt.xlabel("Steps")
 plt.ylabel("Average speed")
-plt.plot(all_speeds)
+plt.plot(all_tracked_agent_speeds, label="Agent 5 speed")
+plt.plot(all_speeds, label="Average speed")
+plt.legend()
+plt.grid()
 plt.show()
--- traffic_simulation/model.py
+++ traffic_simulation/model.py
@@ -4,6 +4,7 @@ from mesa.time import SimultaneousActivation
 from mesa.datacollection import DataCollector
 import random
 
+
 # def compute_traffic_flow(model):
 #     agent_speeds = [agent.speed for agent in model.schedule.agents]
 #     x = sorted(agent_speeds)
@@ -11,86 +12,94 @@ import random
 #     return sum(x)/n
 
 class CarAgent(Agent):
-    """ An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model, acceleration, deceleration, vision_range, randomization, speed_limit):
-        super().__init__(unique_id, model)
-        self.acceleration = acceleration
-        self.deceleration = deceleration
-        self.vision_range = vision_range
-        self.speed = speed_limit
-        self.randomization = randomization
-        self.speed_limit = speed_limit
-        self.state = "Cruising"
+	""" An agent with fixed initial wealth."""
 
-    def perceive(self):
-        p = list(self.pos)
-        p[0] += 1
-        distance_to_precursor = 1000000000
-        for tile in range(0, self.vision_range):
-            tile = (tile,0)
-            if not self.model.grid.is_cell_empty(tile):
-                distance_to_precursor = tile[0]
-                break
-        if distance_to_precursor<self.speed and self.speed>0:
-            self.state = "Braking"
-        elif distance_to_precursor==self.speed and self.speed<self.speed_limit or self.speed == self.speed_limit:
-            self.state = "Cruising"
-        else:
-            self.state = "Accelerating"
+	def __init__(self, unique_id, model, acceleration, vision_range, randomization, speed_limit):
+		super().__init__(unique_id, model)
+		self.acceleration = acceleration
+		self.vision_range = vision_range
+		self.speed = speed_limit
+		self.randomization = randomization
+		self.speed_limit = speed_limit
+		self.iteration = 0
+		self.distance_to_precursor = 0
+		self.state = "Cruising"
 
-    def act(self):
-        if self.state == "Braking":
-            self.speed -= self.deceleration
-        if self.state == "Accelerating":
-            self.speed += self.acceleration
+	def perceive(self):
+		distance_to_precursor = self.vision_range
+		for t in range(self.pos[0]+1, self.vision_range):
+			tile = (t, 0)
+			if self.model.grid.is_cell_empty(tile):
+				distance_to_precursor += 1
+			else:
+				break
 
-        if self.speed<0 :
-            self.speed = 0
-        if self.speed>self.speed_limit:
-            self.speed = self.speed_limit
+		self.distance_to_precursor = distance_to_precursor
 
-    def update(self):
-        pass
+		ran = random.randrange(0, 100)
+		if self.speed > 0 and ran / 100 < self.randomization:
+			self.state = "RandomBraking"
+		elif distance_to_precursor < self.speed and self.speed > 0:
+			self.state = "Braking"
+		elif distance_to_precursor == self.speed and self.speed > 0 or self.speed == self.speed_limit:
+			self.state = "Cruising"
+		else:
+			self.state = "Accelerating"
 
-    def move(self):
-        if self.speed>0 :
-            ran = random.randrange(0, 100)
-            if(ran/100<self.randomization):
-                self.speed -= 1
+	def act(self):
+		if self.state == "Braking":
+			self.speed = self.distance_to_precursor-1
+		if self.state == "RandomBraking":
+			self.speed -= 1
+		if self.state == "Accelerating":
+			self.speed += self.acceleration
 
-        pos = list(self.pos)
-        pos[0] += self.speed
-        self.model.grid.move_agent(self, pos)
+		if self.speed < 0:
+			self.speed = 0
+		if self.speed > self.speed_limit:
+			self.speed = self.speed_limit
 
-    def step(self):
-        self.perceive()
+	def update(self):
+		pass
 
-    def advance(self):
-        self.act()
-        self.move()
-        print(self.unique_id, "::", self.state)
+	def move(self):
+		pos = list(self.pos)
+		pos[0] += self.speed
+		self.model.grid.move_agent(self, pos)
+
+	def step(self):
+		self.perceive()
+
+	def advance(self):
+		self.act()
+		self.move()
+		self.iteration += 1
+		if self.unique_id == 5:
+			print("Step:", self.iteration, "::", "Speed:", str(self.speed) + "/" + str(self.speed_limit),"-->", "Distance:", self.distance_to_precursor ,self.state)
+
 
 class CarModel(Model):
-    """A model with some number of agents."""
-    def __init__(self, n, width, height, acceleration, deceleration, vision_range, randomization, speed_limit):
-        self.num_agents = n
-        self.grid = MultiGrid(width, height, True)
-        
-        self.schedule = SimultaneousActivation(self)
-        self.running = True
+	"""A model with some number of agents."""
 
-        # Create agents
-        for i in range(n):
-            a = CarAgent(i, self, acceleration, deceleration, vision_range, randomization, speed_limit)
-            self.schedule.add(a)
-            # Add the agent to a random grid cell
-            self.grid.place_agent(a, (i,0))
+	def __init__(self, n, width, height, acceleration, vision_range, randomization, speed_limit):
+		self.num_agents = n
+		self.grid = MultiGrid(width, height, True)
 
-        # self.datacollector = DataCollector(
-        #     model_reporters={"Traffic Flow": compute_traffic_flow(self)},
-        #     agent_reporters={"Speed": "speed"}
-        # )
+		self.schedule = SimultaneousActivation(self)
+		self.running = True
 
-    def step(self):
-        # self.datacollector.collect(self)
-        self.schedule.step()
+		# Create agents
+		for i in range(n):
+			a = CarAgent(i, self, acceleration, vision_range, randomization, speed_limit)
+			self.schedule.add(a)
+			# Add the agent to a random grid cell
+			self.grid.place_agent(a, (i, 0))
+
+		# self.datacollector = DataCollector(
+		#     model_reporters={"Traffic Flow": compute_traffic_flow(self)},
+		#     agent_reporters={"Speed": "speed"}
+		# )
+
+	def step(self):
+		# self.datacollector.collect(self)
+		self.schedule.step()
