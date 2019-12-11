72888675ba47ba15d807a77c7b933b898ebabeaf
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2019-12-11 09:44:09 +0100

    changed dict creation for easier editing in the futher

--- traffic_simulation/viz.py
+++ traffic_simulation/viz.py
@@ -15,7 +15,17 @@ def agent_portrayal(agent):
 #Creates grid to be displayed
 grid = CanvasGrid(agent_portrayal, 125, 1, 800, 100)
 
-model_par_dict =  {"n":10, "car_count":20, "height":10,"acceleration":1,"speed_limit":5, "randomization":0.1}
+n = 1000  # Amount of model steps
+car_count = 20  # Amount of cars in model
+height = 1
+acceleration = 1
+speed_limit = 5
+randomization = 0.25
+vision_range = speed_limit*2
+width = n*car_count+vision_range+1
+
+model_par_dict =  {"car_count":car_count, "width":width, "height":height, "acceleration":acceleration,
+                  "vision_range":vision_range ,"speed_limit":speed_limit, "randomization":randomization}
 
 server = ModularServer(CarModel, [grid], "Car Model", model_par_dict)
 server.port = 8521 # The default
