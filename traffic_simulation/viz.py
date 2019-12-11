264a6f93ea6c6430d74645b54dc3267f878ddb4e
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2019-12-11 09:30:18 +0100

    updated viz for updated model

--- traffic_simulation/viz.py
+++ traffic_simulation/viz.py
@@ -13,10 +13,9 @@ def agent_portrayal(agent):
     return portrayal
 
 #Creates grid to be displayed
-grid = CanvasGrid(agent_portrayal, 200, 1, 800, 100)
+grid = CanvasGrid(agent_portrayal, 125, 1, 800, 100)
 
-model_par_dict =  {"n":10, "width":1000, "height":10,"acceleration":1, "deceleration":1, "vision_range":100,
-                   "speed_limit":5, "randomization":0.1}
+model_par_dict =  {"n":10, "car_count":20, "height":10,"acceleration":1,"speed_limit":5, "randomization":0.1}
 
 server = ModularServer(CarModel, [grid], "Car Model", model_par_dict)
 server.port = 8521 # The default
