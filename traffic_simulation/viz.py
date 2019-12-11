79482ce32354f010296ef1f2cdd33d35cb7f00ff
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2019-12-11 12:22:00 +0100

    Last commit was a lie.
    Added a function that changes the color of a agent based on how much they needed to reduce there speed.

--- traffic_simulation/viz.py
+++ traffic_simulation/viz.py
@@ -6,15 +6,29 @@ from model import CarModel
 def agent_portrayal(agent):
     """Holds the parameters for how the agents are displayed"""
     
+    def calculate_color():
+        """Calculates the color for a agent based on the delay they are experiencing"""
+
+        factor_maximum_speed = agent.speed/agent.speed_limit
+        
+        #Inverts the number so that bigger speed reduction for agents equels a higer number
+        color_multiplication = 1 - factor_maximum_speed
+        
+        #Green == Less delay Red == More delay
+        green = int(255 - (color_multiplication * 200))
+        red = int(1 + color_multiplication * 200)
+
+        return '#{:02x}{:02x}{:02x}'.format(red, green,0)
+
+    color = calculate_color()
     portrayal = {"Shape": "circle",
-                 "Color": "red",
                  "Filled": "true",
+                 "Color": color,
                  "Layer": 0,
                  "r": 0.6}
+    
     return portrayal
 
-#Creates grid to be displayed
-grid = CanvasGrid(agent_portrayal, 180, 1, 900, 100)
 
 #Standard starting values
 width = 150 # Road size
@@ -22,6 +36,9 @@ height = 1  # Amount of lanes
 acceleration = 1
 randomization = 0.1
 vision_range = 100
+
+#Creates grid to be displayed
+grid = CanvasGrid(agent_portrayal, 180, 1, 900, 100)
 
 #Values for vizualisation
 model_par_dict =  {"car_count": UserSettableParameter("number", "Car count", 50),
