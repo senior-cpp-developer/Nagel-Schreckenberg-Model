d4fd8e13f9a9b898da923ce8855901e2e162ee71
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2019-12-11 11:14:23 +0100

    added user interaction to viz

--- traffic_simulation/viz.py
+++ traffic_simulation/viz.py
@@ -1,5 +1,6 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
 from model import CarModel
 
 def agent_portrayal(agent):
@@ -13,19 +14,19 @@ def agent_portrayal(agent):
     return portrayal
 
 #Creates grid to be displayed
-grid = CanvasGrid(agent_portrayal, 125, 1, 800, 100)
+grid = CanvasGrid(agent_portrayal, 180, 1, 900, 100)
 
-n = 1000  # Amount of model steps
-car_count = 20  # Amount of cars in model
+width = 150 # Road size
 height = 1
 acceleration = 1
-speed_limit = 5
-randomization = 0.25
-vision_range = speed_limit*2
-width = n*car_count+vision_range+1
+randomization = 0.1
+vision_range = 100
 
-model_par_dict =  {"car_count":car_count, "width":width, "height":height, "acceleration":acceleration,
-                  "vision_range":vision_range ,"speed_limit":speed_limit, "randomization":randomization}
+model_par_dict =  {"car_count": UserSettableParameter("number", "Car count", 50),
+                  "width":width, "height":height,
+                  "acceleration":UserSettableParameter("number", "acceleration", 1),
+                  "vision_range":vision_range ,"speed_limit": UserSettableParameter("number", "max speed", 10),
+                  "randomization":randomization}
 
 server = ModularServer(CarModel, [grid], "Car Model", model_par_dict)
 server.port = 8521 # The default
