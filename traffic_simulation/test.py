d5a4fc3bfad4ec383e2eeecb4aae4d632c175810
Freek <freekgj@hotmail.com>
2019-12-13 16:48:57 +0100

    puntjes op de i
    - 2 grafieken geupdate
    - readme geupdate
    - requirements aangemaakt.

--- traffic_simulation/test.py
+++ traffic_simulation/test.py
@@ -12,42 +12,49 @@ randomization = 0.005
 traffic_occupations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 50]  # Car counts
 speed_limits = [5, 8, 10, 13]
 
-results = []
+results_speed = []
+results_delay = []
 for num, cars in enumerate(traffic_occupations, start=0):
-	print("Running: "+str(cars)+" cars..")
-	results.append([])
-	settings = []
-	for x in speed_limits:
-		settings.append([cars, 150, 1, acceleration, max(speed_limits)*3, randomization, x])
+    print("Running: " + str(cars) + " cars..")
+    results_speed.append([])
+    results_delay.append([])
+    settings = []
+    for x in speed_limits:
+        settings.append([cars, 100, 1, acceleration, max(speed_limits) * 3, randomization, x])
 
-	for num_setting, setting in enumerate(settings, start=0):
-		results[num].append([])
-		all_speeds = []
-		model = CarModel(*setting)
-		for i in range(n):
-			model.step()
+    for num_setting, setting in enumerate(settings, start=0):
+        results_speed[num].append([])
+        results_delay[num].append([])
+        all_delay = []
+        all_speeds = []
+        model = CarModel(*setting)
+        for i in range(n):
+            model.step()
 
-			# Store the results
-			total = 0
-			for agent in model.schedule.agents:
-				total += agent.speed
-			ave_speed = total/model.num_agents
-			all_speeds.append(abs(setting[6]-ave_speed))
-			# all_speeds.append(ave_speed)
-		print(num_setting, "-->", setting, "::::", "||STD:", round(np.std(all_speeds), 2), "||AVE:", round(np.mean(all_speeds), 2), "||MOD:", stats.mode(all_speeds)[0])
-		results[num][num_setting] = np.mean(all_speeds)
+            # Store the results
+            total = 0
+            for agent in model.schedule.agents:
+                total += agent.speed
+            ave_speed = total / model.num_agents
+            all_delay.append(abs(setting[6] - ave_speed))
+            all_speeds.append(ave_speed)
+        results_speed[num][num_setting] = np.mean(all_speeds)
+        results_delay[num][num_setting] = np.mean(all_delay)
 		# plt.plot(all_speeds, label="Snelheidslimiet: "+str(setting[6]))
 
-plt.plot(traffic_occupations, results)
-plt.xticks(traffic_occupations)
+def plot_line(x_data, y_data, y_label):
+	plt.plot(x_data, y_data)
+	plt.xticks(x_data)
+	plt.title("Effect van wegbezetting en snelheidslimiet op verkeersflow")
+	plt.xlabel("Weg bezetting (%)")
+	plt.ylabel(y_label)
+	# plt.xlim(0, len(traffic_occupations))
+	labels = []
+	for x in settings:
+		labels.append("Snelheidslimiet: " + str(x[6]))
+	plt.legend(labels)
+	plt.grid()
+	plt.show()
 
-plt.title("Effect van wegbezetting en snelheidslimiet op verkeersflow")
-plt.xlabel("Weg bezetting (%)")
-plt.ylabel("Gemiddelde vertraging")
-# plt.xlim(0, len(traffic_occupations))
-labels = []
-for x in settings:
-	labels.append("Snelheidslimiet: "+str(x[6]))
-plt.legend(labels)
-plt.grid()
-plt.show()
+plot_line(traffic_occupations, results_delay, "Gemiddelde vertraging")
+plot_line(traffic_occupations, results_speed, "Gemiddelde snelheid")
--- traffic_simulation/viz.py
+++ traffic_simulation/viz.py
@@ -1,25 +1,25 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
-from mesa.visualization.modules import ChartModule
 from model import CarModel
+
 
 def agent_portrayal(agent):
     """Holds the parameters for how the agents are displayed"""
-    
+
     def calculate_color():
         """Calculates the color for a agent based on the amount they reduced there speed from maximum"""
 
-        factor_maximum_speed = agent.speed/agent.speed_limit
-        
-        #Inverts the number so that bigger speed reduction for agents equels a higer number
+        factor_maximum_speed = agent.speed / agent.speed_limit
+
+        # Inverts the number so that bigger speed reduction for agents equels a higer number
         color_multiplication = 1 - factor_maximum_speed
-        
-        #Green == Less delay Red == More delay
+
+        # Green == Less delay Red == More delay
         green = int(255 - (color_multiplication * 250))
         red = int(1 + color_multiplication * 200)
 
-        return '#{:02x}{:02x}{:02x}'.format(red, green,0)
+        return '#{:02x}{:02x}{:02x}'.format(red, green, 0)
 
     color = calculate_color()
     portrayal = {"Shape": "circle",
@@ -27,26 +27,26 @@ def agent_portrayal(agent):
                  "Color": color,
                  "Layer": 0,
                  "r": 0.6}
-    
+
     return portrayal
 
 
-#Standard starting values
-width = 150 # Road size
+# Standard starting values
+width = 150  # Road size
 height = 1  # Amount of lanes
 
-#Creates grid to be displayed
+# Creates grid to be displayed
 grid = CanvasGrid(agent_portrayal, width, 1, 900, 100)
 
-#Values for vizualisation
-model_par_dict =  {"car_count": UserSettableParameter("number", "Car count", 25),
-                  "width":width, "height":height,
-                  "acceleration":UserSettableParameter("number", "acceleration", 1),
-                  "vision_range":UserSettableParameter("number", "vision_range", 100),
+# Values for vizualisation
+model_par_dict = {"car_count": UserSettableParameter("number", "Car count", 25),
+                  "width": width, "height": height,
+                  "acceleration": UserSettableParameter("number", "acceleration", 1),
+                  "vision_range": UserSettableParameter("number", "vision_range", 100),
                   "speed_limit": UserSettableParameter("number", "max speed", 10),
                   "randomization": UserSettableParameter("number", "randomization", 0.05)}
 
 server = ModularServer(CarModel, [grid], "Car Model", model_par_dict)
 
-server.port = 8521 # The default
+server.port = 8521  # The default
 server.launch()
